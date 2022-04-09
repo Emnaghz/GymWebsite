38d7a94130803e1c0a02ce4736ddebfc694c430c
Emnaghz <emnaghzaiel176@gmail.com>
2022-04-09 23:44:59 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,161 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UFIT Website</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="fontawesome-free-5.15.3-web/css/all.min.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="logo"><span>U</span>FIT</div>
+            <ul class="links">
+                <li><a href="#">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#services">Services</a></li>
+                <li><a href="#trainers">Trainers</a></li>
+                <li><a href="#">Plan</a></li>
+                <li><a href="#">Register</a></li>
+            </ul>
+        </nav>
+        <div class="content">
+            <h1>Time To Start Your Journey</h1>
+            <button class="btn">Get started</button>
+        </div>
+    </header>
+
+    <section class="about" id="about">
+        <div class="row">
+            <div class="image">
+                <img src="./images/img2.jpg" alt="">
+            </div>
+            <div class="content">
+                <div class="title">About <span>U</span>FIT</div>
+                <p>A state of the art gym for fitness lovers with a passion for variety. After noticing we couldn't find a studio that offered enough to consistently motivate women, we decided to create one! UFIT is a fitness facility that provides a mix of instructor-led classes for improved strength, endurance, flexibility, and wellness. Mix and match weight training, boot camp, hot yoga, HIIT, hot barre, pilates, functional fitness, and more.           
+                </p>
+            </div>
+        </div>
+    </section>
+
+    <section class="services" id="services">
+        <div class="title">Our <span>Services</span></div>
+        <div class="container">
+            <div class="box">
+                <img src="./images/cardio.jpg">
+                <div class="info">
+                    <h3>Cardio</h3>
+                    <p>Cardio is a great way to exercise for overall health benefits and will support your weight loss efforts. However, to speed up weight loss and maintain the weight you have lost its important to do some form of resistance exercise. </p>
+                </div>
+            </div>
+            <div class="box">
+                <img src="./images/yoga1.jpg">
+                <div class="info">
+                    <h3>Yoga</h3>
+                    <p>Yoga is a type of exercise in which you move your body into various positions in order to become more fit or flexible, to improve your breathing, and to relax your mind.</p>
+                </div>
+            </div>
+            <div class="box">
+                <img src="./images/pilate.jpg">
+                <div class="info">
+                    <h3>Pilates</h3>
+                    <p>Pilates exercises the body as a whole evenly and symmetrically, which is the best way to develop your core strength and rehabilitate from previous injuries.</p>
+                </div>
+            </div>
+            <div class="box">
+                <img src="./images/zumba.jpg">
+                <div class="info">
+                    <h3>Zumba</h3>
+                    <p>Zumba targets lots of different muscle groups at once for total body toning. Boosts your heart health. You not only get aerobic benefits (it really gets your heart rate up), you also get anaerobic benefits the kind that help you maintain a good cardiovascular respiratory system. Helps you de-stress.</p>
+                </div>
+            </div>
+            <div class="box">
+                <img src="./images/box.jpg">
+                <div class="info">
+                    <h3>boxing</h3>
+                    <p>Boxing is very much about providing an uplifting environment where athletes can decompress, learn, and have fun around peers and positive leaders. It encourages individuality and self-worth.</p>
+                </div>
+            </div>
+            <div class="box">
+                <img src="./images/lifting.jpg">
+                <div class="info">
+                    <h3>weight lifting</h3>
+                    <p>Weight lifting isn't just about bulking up and building muscle mass, the experts say. Its benefits include improved posture, better sleep, gaining bone density, maintaining weight loss, boosting metabolism, lowering inflammation and staving off chronic disease, among a laundry list of positives.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Start Trainers -->
+    <section class="our-trainers">
+        <div class="title">Our <span>Trainers</span></div>
+    
+        <div class="main-trainer">
+            <div class="inner-trainer">
+                <img src="images/coach1.jpg" alt="">
+            </div>
+    
+            <div class="inner-trainer">
+                <div class="trainer-content">
+                    <h2>Trainers</h2>
+                    <span>our builders</span>
+                    <hr>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, dolorem ipsum odio minima iste enim quibusdam error vero quia voluptate accusamus cupiditate, mollitia, dolorum est deleniti nihil fugit ipsam expedita?</p>
+                </div>
+            </div>
+    
+            <div class="inner-trainer">
+                <img src="images/coach2.jpg" alt="">
+            </div>
+    
+            <div class="inner-trainer">
+                <div class="trainer-content">
+                    <h2>Trainers</h2>
+                    <span>our builders</span>
+                    <hr>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, dolorem ipsum odio minima iste enim quibusdam error vero quia voluptate accusamus cupiditate, mollitia, dolorum est deleniti nihil fugit ipsam expedita?</p>
+                </div>
+            </div>
+            <div class="inner-trainer">
+                <img src="images/coach3.jpg" alt="">
+            </div>
+            <div class="inner-trainer">
+                <div class="trainer-content">
+                    <h2>Trainers</h2>
+                    <span>our builders</span>
+                    <hr>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, dolorem ipsum odio minima iste enim quibusdam error vero quia voluptate accusamus cupiditate, mollitia, dolorum est deleniti nihil fugit ipsam expedita?</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- reviews -->
+<div class="customer-review">
+    <div class="title">customer <span>review</span></div>
+ <div class="main-customer">
+    <div class="inner-customer">
+        <img src="images/cardio.jpg" alt="">
+        <p>AMAZING GYM, AMAZING WORKOUTS! The staff here is incredibly friendly and extremely qualified! They know how to push you to the limit in the best way possible with your workouts. You will not leave disappointed!</p>
+    </div>
+
+    <div class="inner-customer">
+        <img src="images/coach1.jpg" alt="">
+        <p> Excellent personal training. Trainers who really care and go above and beyond to help you achieve your goals. Small and private gym that feels more personal. Highly recommended!</p>
+    </div>
+
+    <div class="inner-customer">
+        <img src="images/box.jpg" alt="">
+        <p>This is a fantastic gym!! All the trainers are super nice and take an interest in you no matter what fitness level you're at. I really like how they give me tips and tricks to get the most out of every workout.</p>
+    </div>
+ </div>
+</div>
+
+
+</body>
+</html>
